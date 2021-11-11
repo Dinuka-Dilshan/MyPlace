cfc413ae02550cdf78650004af869f1425355081
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-11-12 00:57:40 +0530

    nav drawer

--- src/shared/Navigation/NavDrawer/NavDrawer.jsx
+++ src/shared/Navigation/NavDrawer/NavDrawer.jsx
@@ -1,30 +1,27 @@
 import "./NavDrawer.css";
 import NavItems from "../NavItems/NavItems";
-import Logo from "../Logo/Logo";
 
 const listStyleOveride = {
   display: "block",
-  marginLeft:'0'
+  marginLeft: "0"
 };
 
 const listItemStyleOveride = {
   marginTop: "3rem",
-  fontSize: "1rem"
-  
+  fontSize: "1.4rem",
 };
-
-const logoWrapperOverride = {
-    marginLeft:'1.5rem'
-}
 
 const NavDrawer = (props) => {
   return (
     <div className="nav-drawer">
-      <Logo logoWrapper={logoWrapperOverride}/>
-      <NavItems
-        listStyle={listStyleOveride}
-        listItemStyle={listItemStyleOveride}
-      />
+      <div className="nav-drawer-content-wrapper">
+        <div className="nav-drawer-content">
+          <NavItems
+            listStyle={listStyleOveride}
+            listItemStyle={listItemStyleOveride}
+          />
+        </div>
+      </div>
     </div>
   );
 };
